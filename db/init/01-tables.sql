a862d263d172671b43be6790391039fa8fc9fe22
Glenn Olsson <glennholsson@gmail.com>
2024-02-08 17:24:43 +0000

    Add users table

--- db/init/01-tables.sql
+++ db/init/01-tables.sql
@@ -5,3 +5,13 @@ CREATE TABLE subscribers (
 	created_at TIMESTAMP DEFAULT current_timestamp,
 	updated_at TIMESTAMP DEFAULT current_timestamp
 );
+
+CREATE TABLE users (
+	id SERIAL PRIMARY KEY,
+	user VARCHAR(128) UNIQUE NOT NULL,
+	password_hash VARCHAR(256) NOT NULL,
+	authority SMALLINT NOT NULL, -- Higher authority can do more
+	created_at TIMESTAMP DEFAULT current_timestamp,
+	updated_at TIMESTAMP DEFAULT current_timestamp	
+);
+
