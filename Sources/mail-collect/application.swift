f7cc18755ddca84a6adcad1626a7147d4028995d
Glenn Olsson <glennholsson@gmail.com>
2024-02-12 02:45:05 +0100

    Add CORS middleware

---
+++ Sources/mail-collect/application.swift
@@ -0,0 +1,35 @@
+import Vapor
+
+func configure(app: Application) {
+	let corsConfiguration = CORSMiddleware.Configuration(
+		allowedOrigin: .all,
+		allowedMethods: [.GET, .POST, .PUT, .OPTIONS, .DELETE, .PATCH],
+		allowedHeaders: [.accept, .authorization, .contentType, .origin, .xRequestedWith, .userAgent, .accessControlAllowOrigin]
+	)
+	let cors = CORSMiddleware(configuration: corsConfiguration)
+	app.middleware.use(cors, at: .beginning)
+
+	// Otherwise cannot be reached from outside docker container
+	app.http.server.configuration.hostname = "0.0.0.0"
+
+	let databaseConfig = createDatabaseConfig(
+		username: getEnvironment(.DATABASE_USER)!, 
+		password: getEnvironment(.DATABASE_PASSWORD)!, 
+		database: getEnvironment(.DATABASE)!
+	)
+
+
+	useDatabase(
+		on: app,
+		with: databaseConfig
+	)
+
+	let authenticatedApp = app.grouped(User.authenticator())
+
+	defer { 
+		print("Shutting down...")
+		app.shutdown() 
+	}
+
+	registerRoutes(auth: authenticatedApp, nonAuth: app)
+}
--- Sources/mail-collect/main.swift
+++ Sources/mail-collect/main.swift
@@ -1,34 +1,11 @@
 import Vapor
 import Foundation
 
-print("Starting")
-
 var env = try Environment.detect()
 try LoggingSystem.bootstrap(from: &env)
 
 let app = Application(env)
 
-// Otherwise cannot be reached from outside docker container
-app.http.server.configuration.hostname = "0.0.0.0"
-
-
-let databaseConfig = createDatabaseConfig(
-	username: getEnvironment(.DATABASE_USER)!, 
-	password: getEnvironment(.DATABASE_PASSWORD)!, 
-	database: getEnvironment(.DATABASE)!
-)
-useDatabase(
-	on: app,
-	with: databaseConfig
-)
-
-let authenticatedApp = app.grouped(User.authenticator())
-
-defer { 
-	print("Shutting down...")
-	app.shutdown() 
-}
-
-registerRoutes(auth: authenticatedApp, nonAuth: app)
+configure(app)
 
 try! app.run()
