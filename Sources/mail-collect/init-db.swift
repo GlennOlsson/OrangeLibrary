406c36cd6dfd699a5bf53815ed7e81e9c10f54f9
Glenn Olsson <glennholsson@gmail.com>
2024-02-20 23:18:48 +0000

    Fix tests

--- Sources/mail-collect/init-db.swift
+++ Sources/mail-collect/init-db.swift
@@ -5,6 +5,7 @@ func initDatabase(db: Database) async throws {
 		try await db.schema(Subscriber.schema)
 			.field("id", .int, .identifier(auto: true))
 			.field("email", .string)
+			.field("real_name", .string)
 			.field("created_at", .datetime)
 			.field("updated_at", .datetime)
 			.ignoreExisting()
